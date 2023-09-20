eab5ab63750b4bf011b63cbfe6d8e2bb9aa8b675
brother bro <rektdevvv@gmail.com>
2023-09-20 15:34:04 +0300

    beginning sk8shop

---
+++ src/components/Currency.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Currency=()=>{
+    return(
+        <div>
+            <h1>Currency</h1>
+        </div>
+    )
+} 
+export default Currency;
+
+
---
+++ src/components/UserMenu.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Cart from './Cart'
+import Currecncy from './Currency'
+import User from './User'
+
+const UserMenu=()=>{
+    return(
+        <div>
+            <h1>UserMenu</h1>
+            <Cart />
+            <Currecncy />
+            <User />
+            <Cart />
+
+        </div>
+    )
+} 
+export default UserMenu;
+
+
