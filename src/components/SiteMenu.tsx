eaa29a7edee57742dbd9ebca1af693f90f65a745
brother bro <rektdevvv@gmail.com>
2023-09-20 22:10:27 +0300

    g

--- src/components/SiteMenu.tsx
+++ src/components/SiteMenu.tsx
@@ -3,6 +3,14 @@ import React from 'react'
 const SiteMenu=()=>{
     return(
         <div>
+             <nav className="menu">
+      <ul className="menu-list">
+        <li className="menu-item"><a href="/collections">Collections</a></li>
+        <li className="menu-item"><a href="/accessories">Accessories</a></li>
+        <li className="menu-item"><a href="/accessories">HardGoods</a></li>
+
+      </ul>
+    </nav>
         </div>
     )
 } 
