18a387abf7331a37edaef31bbe99ead566e1054a
brother bro <rektdevvv@gmail.com>
2023-09-20 15:59:17 +0300

    new components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '../App.css';
 import Cart from './Cart'
 import Currecncy from './Currency'
 import User from './User'
