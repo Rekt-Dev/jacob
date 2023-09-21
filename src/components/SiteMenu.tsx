c990af22f14cbdbc66cb1336fd6e4cd0161ed8b4
brother bro <rektdevvv@gmail.com>
2023-09-21 18:00:01 +0300

    react router

--- src/components/SiteMenu.tsx
+++ src/components/SiteMenu.tsx
@@ -5,10 +5,18 @@ const SiteMenu=()=>{
     return(
         <div>
              <nav className="menu">
+
       <ul className="menu-list">
-        <li className="menu-item"><a href="/collections">Collections</a></li>
-        <li className="menu-item"><a href="/accessories">Accessories</a></li>
-        <li className="menu-item"><a href="/accessories">HardGoods</a></li>
+
+        <li className="menu-item">
+          <Link to="/collections">Collections</a>
+          </li>
+        <li className="menu-item">
+          <Link to="/accessories">Accessories</a>
+          </li>
+        <li className="menu-item">
+          <Link to="/accessories">HardGoods</a>
+          </li>
 
       </ul>
     </nav>
