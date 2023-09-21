054742b7a832faf2b4da4967bdb72706a4f65713
brother bro <rektdevvv@gmail.com>
2023-09-21 20:59:32 +0300

    yes

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import search from '../assets/search.png'
+import { Link } from 'react-router-dom';
 const Search=()=>{
     return(
         <div>
-
+<Link to = "/Search">
             <img src={search} alt="Search" />
-
+</Link>
         </div>
     )
 } 
