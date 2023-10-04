debd6f34fb8226b704be7d9ca24bc245ab1ad21d
brother bro <rektdevvv@gmail.com>
2023-10-04 14:50:16 +0300

    smaller-text class

--- src/components/Ofear.tsx
+++ src/components/Ofear.tsx
@@ -2,7 +2,7 @@ import react from   'react'
 
 export default function Ofear(){
     return(
-        <div>
+        <div className='smaller-text'>
 <p>&copy; {new Date().getFullYear()} <span className="smaller-text">0Fear Designs</span>. All rights reserved.</p>
         </div>
     )
