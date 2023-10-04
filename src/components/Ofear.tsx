2e39300d3c7b8d9e2f8a648d4bc90083a2e97d80
brother bro <rektdevvv@gmail.com>
2023-10-04 23:55:52 +0300

    .

--- src/components/Ofear.tsx
+++ src/components/Ofear.tsx
@@ -2,7 +2,7 @@ import react from   'react'
 
 export default function Ofear(){
     return(
-        <div className='smaller-text'>
+        <div className='smaller-text padding'>
 <p>&copy; {new Date().getFullYear()} <span className="smaller-text">0Fear Designs</span>. All rights reserved.</p>
         </div>
     )
